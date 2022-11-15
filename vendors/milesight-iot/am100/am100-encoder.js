053b1ee526d379429f2249dd2e129e78351df378
Sway <sway@milesight.com>
2022-11-15 09:15:29 +0800

    feat: support AM103/AM104/AM107 and AM307/AM308/AM319

---
+++ vendors/milesight-iot/am100/am100-encoder.js
@@ -0,0 +1,14 @@
+/**
+ * Payload Eecoder for Chirpstack and Milesight network server
+ *
+ * Copyright 2022 Milesight IoT
+ *
+ * @product AM100
+ * @params
+ *     - fPort: 85
+ *     - obj: {"key": "value"}
+ */
+function Encode(fPort, obj) {
+  var encoded = [];
+  return encoded;
+}
---
+++ vendors/milesight-iot/am300/am300-encoder.js
@@ -0,0 +1,14 @@
+/**
+ * Payload Eecoder for Chirpstack and Milesight network server
+ *
+ * Copyright 2022 Milesight IoT
+ *
+ * @product AM300 Series
+ * @params
+ *     - fPort: 85
+ *     - obj: {"key": "value"}
+ */
+function Encode(fPort, obj) {
+    var encoded = [];
+    return encoded;
+}
