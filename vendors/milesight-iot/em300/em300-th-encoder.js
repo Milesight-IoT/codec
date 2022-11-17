3478dfa695f662c3d12c427ad6ffec27fac9abf3
Sway <sway@milesight.com>
2022-11-17 16:03:59 +0800

    feat: support em300-th

---
+++ vendors/milesight-iot/em300/em300-th-encoder.js
@@ -0,0 +1,14 @@
+/**
+ * Payload Eecoder for Chirpstack and Milesight network server
+ *
+ * Copyright 2022 Milesight IoT
+ *
+ * @product EM300-TH
+ * @params
+ *     - fPort: 85
+ *     - obj: {"key": "value"}
+ */
+function Encode(fPort, obj) {
+  var encoded = [];
+  return encoded;
+}
